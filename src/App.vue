0835e3b00558ba96ed025510be648461ab0a4742
xiehaopei <15907678645@163.com>
2020-07-15 17:59:42 +0800

    完成form组件开发

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <router-link to="/form">form</router-link>
-    <router-link to="/">/</router-link>
     <router-view></router-view>
   </div>
 </template>
@@ -9,5 +8,5 @@
 <script>
 export default {
   name: 'App'
-}
+};
 </script>
--- src/components/form/form.vue
+++ src/components/form/form.vue
@@ -8,15 +8,62 @@
 </template>
 
 <script>
-  export default {
-    name:'iForm',
-    props:{
-      model:{
-        type: Object
-      },
-      rules:{
-        type: Object
-      }
+export default {
+  name: 'iForm',
+  data() {
+    return {
+      fields: []
+    };
+  },
+  props: {
+    model: {
+      type: Object
+    },
+    rules: {
+      type: Object
+    }
+  },
+  provide() {
+    return {
+      form: this
+    };
+  },
+  created() {
+    this.$on('on-form-item-add', field => {
+      if (field) this.fields.push(field);
+    });
+    this.$on('on-form-item-remove', field => {
+      if (field.prop) this.fields.splice(this.fields.indexOf(field), 1);
+    });
+  },
+  methods: {
+    // 公开方法：全部重置数据
+    resetFields() {
+      this.fields.forEach(field => {
+        field.resetField();
+      });
+    },
+    // 公开方法：全部校验数据，支持 Promise
+    validate(callback) {
+      return new Promise(resolve => {
+        let valid = true;
+        let count = 0;
+        this.fields.forEach(field => {
+          field.validate('', errors => {
+            if (errors) {
+              valid = false;
+            }
+            if (++count === this.fields.length) {
+              // 全部完成
+              resolve(valid);
+              if (typeof callback === 'function') {
+                callback(valid);
+              }
+            }
+          });
+        });
+      });
     }
   }
+};
 </script>
